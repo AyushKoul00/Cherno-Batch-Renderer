943c2d4901b5aaffd6d17a80abb8478803d4c7bd
Ayush Koul <46340004+AyushKoul00@users.noreply.github.com>
2021-11-05 10:11:44 +0530

    Update Renderer.cpp

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -4,7 +4,7 @@
 #include <array>
 #include <glad/glad.h>
 
-static const size_t MaxQuadCount = 2;
+static const size_t MaxQuadCount = 1000;
 static const size_t MaxVertexCount = MaxQuadCount * 4;
 static const size_t MaxIndexCount = MaxQuadCount * 6;
 static const size_t MaxTextures = 32;
@@ -43,8 +43,10 @@ static RendererData s_Data;
 void Renderer::Init()
 {
 	if (s_Data.QuadBuffer) return;
-
-	//glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &MaxTextures);
+	
+	//GLint ans;
+	//glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &ans);
+	//std::cout << "Max Textures: " << ans << std::endl;
 
 	s_Data.QuadBuffer = new Vertex[MaxVertexCount];
 
@@ -55,21 +57,21 @@ void Renderer::Init()
 	glBindBuffer(GL_ARRAY_BUFFER, s_Data.QuadVB);
 	glBufferData(GL_ARRAY_BUFFER, MaxVertexCount * sizeof(Vertex), nullptr, GL_DYNAMIC_DRAW);
 
-	glEnableVertexArrayAttrib(s_Data.QuadVA, 0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, Position));
+	glEnableVertexArrayAttrib(s_Data.QuadVA, 0);
 	
-	glEnableVertexArrayAttrib(s_Data.QuadVA, 1);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, Color));
+	glEnableVertexArrayAttrib(s_Data.QuadVA, 1);
 	
-	glEnableVertexArrayAttrib(s_Data.QuadVA, 2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, TexCoords));
+	glEnableVertexArrayAttrib(s_Data.QuadVA, 2);
 
-	glEnableVertexArrayAttrib(s_Data.QuadVA, 3);
 	glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, TexIndex));
+	glEnableVertexArrayAttrib(s_Data.QuadVA, 3);
 	
 	uint32_t indices[MaxIndexCount];
 	uint32_t offset = 0;
-	for (size_t i = 0; i < MaxIndexCount; i+=6)
+	for (size_t i = 0; i < MaxIndexCount; i+= 6)
 	{
 		indices[i + 0] = 0 + offset;
 		indices[i + 1] = 1 + offset;
@@ -88,12 +90,13 @@ void Renderer::Init()
 
 	//1x1 white Texture
 	glCreateTextures(GL_TEXTURE_2D, 1, &s_Data.WhiteTexture);
+	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, s_Data.WhiteTexture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	const uint32_t color = 0xffffffff;
+	uint32_t color = 0xffffffff;
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &color);
 	
 	s_Data.TextureSlots[0] = s_Data.WhiteTexture;
@@ -127,14 +130,16 @@ void Renderer::Flush()
 	for (uint32_t i = 0; i < s_Data.TextureSlotIndex; ++i)
 		glBindTextureUnit(i, s_Data.TextureSlots[i]);
 
+	//glUseProgram(m_Shader->GetRendererID());
 	glBindVertexArray(s_Data.QuadVA);
-	glDrawElements(GL_TRIANGLES, s_Data.IndexCount, GL_UNSIGNED_BYTE, nullptr);
+	glDrawElements(GL_TRIANGLES, s_Data.IndexCount, GL_UNSIGNED_INT, nullptr);
 	++s_Data.RenderStats.DrawCount;
 
 	s_Data.IndexCount = 0;
 	s_Data.TextureSlotIndex = 1;
 }
 
+//17:48
 void Renderer::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
 {
 	if (s_Data.IndexCount >= MaxIndexCount)
@@ -150,33 +155,33 @@ void Renderer::DrawQuad(const glm::vec2& position, const glm::vec2& size, const 
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 0.0f, 0.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x + size.x, position.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 1.0f, 0.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x + size.x, position.y + size.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 1.0f, 1.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x, position.y + size.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 0.0f, 1.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.IndexCount += 6;
-	++s_Data.RenderStats.QuadCount;
+	s_Data.RenderStats.QuadCount++;
 }
 
 void Renderer::DrawQuad(const glm::vec2& position, const glm::vec2& size, uint32_t textureID)
 {
-	if (s_Data.IndexCount >= MaxIndexCount || s_Data.TextureSlotIndex >= MaxTextures)
+	if (s_Data.IndexCount >= MaxIndexCount || s_Data.TextureSlotIndex > 31)
 	{
 		EndBatch();
 		Flush();
@@ -199,7 +204,7 @@ void Renderer::DrawQuad(const glm::vec2& position, const glm::vec2& size, uint32
 	{
 		textureIndex = (float)s_Data.TextureSlotIndex;
 		s_Data.TextureSlots[s_Data.TextureSlotIndex] = textureID;
-		++s_Data.TextureSlotIndex;
+		s_Data.TextureSlotIndex++;
 	}
 
 
@@ -207,28 +212,28 @@ void Renderer::DrawQuad(const glm::vec2& position, const glm::vec2& size, uint32
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 0.0f, 0.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x + size.x, position.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 1.0f, 0.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x + size.x, position.y + size.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 1.0f, 1.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.QuadBufferPtr->Position = { position.x, position.y + size.y, 0.0f };
 	s_Data.QuadBufferPtr->Color = color;
 	s_Data.QuadBufferPtr->TexCoords = { 0.0f, 1.0f };
 	s_Data.QuadBufferPtr->TexIndex = textureIndex;
-	++s_Data.QuadBufferPtr;
+	s_Data.QuadBufferPtr++;
 
 	s_Data.IndexCount += 6;
-	++s_Data.RenderStats.QuadCount;
+	s_Data.RenderStats.QuadCount++;
 }
 
 const Renderer::Stats& Renderer::GetStats()
@@ -238,5 +243,5 @@ const Renderer::Stats& Renderer::GetStats()
 
 void Renderer::ResetStats()
 {
-	memset(&s_Data.RenderStats, 0, sizeof(Stats));
+	memset(&s_Data.RenderStats, 0, sizeof(Renderer::Stats));
 }
